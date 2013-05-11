28a7a92f5a568ca1551a930f70149b97d2f3b9e5
Carlos Rios <carlos@blackeaglevision.co.uk>
2013-05-11 14:28:34 +0100

    categorizations not working

--- db/migrate/20130510182014_add_categorizations.rb
+++
@@ -1,9 +0,0 @@
-class AddCategorizations < ActiveRecord::Migration
-  def change
-    create_table :categorizations do |t| 
-      t.integer :category_id
-      t.integer :video_post_id
-    end
-  add_index :categorizations, [:category_id, :video_post_id]
-  end
-end
--- db/migrate/20130510184533_create_categorization.rb
+++
@@ -1,8 +0,0 @@
-class CreateCategorization < ActiveRecord::Migration
-  def change
-    create_table :categorization do |t| 
-      t.integer :category_id
-      t.integer :video_post_id
-    end
-  end
-end
